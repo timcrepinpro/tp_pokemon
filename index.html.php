1a6f697db0f1a2185bf29274aa45e504a26a2c41
timcrepinpro <tim.crepin@edouardgand.fr>
2026-02-05 14:06:59 +0100

    initial commit

---
+++ index.html.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"> <!-- Encodage -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Responsive -->
+    <title>Ma page web</title> <!-- Titre affiché dans l'onglet -->
+    <link rel="stylesheet" href="style.css"> <!-- Feuille de style -->
+    <script src="script.js" defer></script> <!-- Script JS -->
+</head>
+<body>
+    
+
+
+<?php
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
